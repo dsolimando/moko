093d2b5f19e6bee11b757ce83144cac726cf41e3
Solimando Damien <dsolimando@gmail.com>
2018-04-10 13:10:11 +0200

    Handle no navigation bar stack navigator

--- js/stack-navigator.js
+++ js/stack-navigator.js
@@ -7,8 +7,18 @@ export class StackState {
     set viewController (viewController) { this.viewController = viewController}
 }
 
+function handleNavigationBar (state) {
+    console.log(!(state.options && state.options.navigationBar != undefined && state.options.navigationBar == false))
+    return !(state.options && state.options.navigationBar != undefined && state.options.navigationBar == false)
+}
+
 export class StackNavigator {
 
+    /**
+     * 
+     * @param {string} transition 
+     * @param {StackState} rootState 
+     */
     constructor(transition, rootState) {
         this.stack = [rootState]
         this.passiveController = null
@@ -26,7 +36,7 @@ export class StackNavigator {
             this.passiveController.render().remove()
         }
         
-        this.showCurrentViewAndHidePrevious(state.viewController)
+        this.showCurrentViewAndHidePrevious(state)
     }
 
     pop() {
@@ -40,7 +50,11 @@ export class StackNavigator {
         this.activeController = new rootState.viewController(rootState.props)
         this.activeController.navigator = this
 
-        this.insertNavigationBarForPush()
+        const handleNavBar = handleNavigationBar(rootState)
+        
+        if (handleNavBar)
+            this.insertNavigationBarForPush()
+        
         const rootView = this.activeController.render()
         
         if (this.transition == 'cover-vertical') {
@@ -53,14 +67,13 @@ export class StackNavigator {
         rootView.style.transition = 'transform 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
         document.body.appendChild(rootView)
 
-        rootView.addEventListener('dom-complete', event => {
-            alert('toto')
-        })
         setTimeout (_ => {
-            const navigationBar = rootView.querySelector('scell-navigation-bar')
-            navigationBar.leftSlotAssignedNode.hide()
-            navigationBar.shadowRoot.querySelector('.center-zone > span').style.transition = 'transform 0.2s  cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-            rootView.style.paddingTop = getComputedStyle(navigationBar).height
+            if (handleNavBar) {
+                const navigationBar = rootView.querySelector('scell-navigation-bar')
+                navigationBar.leftSlotAssignedNode.hide()
+                navigationBar.shadowRoot.querySelector('.center-zone > span').style.transition = 'transform 0.2s  cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+                rootView.style.paddingTop = getComputedStyle(navigationBar).height
+            }
         },100)
     }
 
@@ -101,53 +114,70 @@ export class StackNavigator {
         this.activeController.render().querySelector('scell-navigation-bar').navigator = this
     }
 
-    showCurrentViewAndHidePrevious (currentControllerClass) {
+    /**
+     * 
+     * @param {StackState} state 
+     */
+    showCurrentViewAndHidePrevious (state) {
+        const currentControllerClass = state.viewController
         this.passiveController = this.activeController
-        this.activeController = new currentControllerClass(this.activeController.props)
+        this.activeController = new currentControllerClass(state.props)
         this.activeController.navigator = this
 
-        this.insertNavigationBarForPush()
+        const handleNavBar = handleNavigationBar(state)
+
+        if (handleNavBar)
+            this.insertNavigationBarForPush()
 
         const passiveView = this.passiveController.render()
         const activeView = this.activeController.render()
 
         document.body.appendChild(activeView)
 
-        const navBarPassiveView = passiveView.querySelector('scell-navigation-bar')
-        activeView.style.paddingTop = getComputedStyle(navBarPassiveView).height
-        const navBarActiveView = activeView.querySelector('scell-navigation-bar')
-        const activeTitleSpan = navBarActiveView.shadowRoot.querySelector('.center-zone > span')
-        const activeBackSpan = navBarActiveView.shadowRoot.querySelector('.back-zone > span')
+        let navBarActiveView,activeTitleSpan,activeBackSpan,passiveTitleSpan,backButton
         
-        activeBackSpan.parentElement.onclick = event => {
-            this.pop()
+        if (handleNavBar) {
+            const navBarPassiveView = passiveView.querySelector('scell-navigation-bar')
+            activeView.style.paddingTop = getComputedStyle(navBarPassiveView).height
+            navBarActiveView = activeView.querySelector('scell-navigation-bar')
+            activeTitleSpan = navBarActiveView.shadowRoot.querySelector('.center-zone > span')
+            activeBackSpan = navBarActiveView.shadowRoot.querySelector('.back-zone > span')
+            
+            activeBackSpan.parentElement.onclick = event => {
+                this.pop()
+            }
+
+            passiveTitleSpan = navBarPassiveView.shadowRoot.querySelector('.center-zone > span')
+            backButton = navBarActiveView.leftSlotAssignedNode
         }
 
-        const passiveTitleSpan = navBarPassiveView.shadowRoot.querySelector('.center-zone > span')
-        const backButton = navBarActiveView.leftSlotAssignedNode
-        
         if (this.transition == 'cover-vertical') {
             activeView.style.transform = 'translateY(10%)'
             activeView.style.opacity = 0.5
         } else {
             activeView.style.transform = 'translateX(100%)'
-            activeTitleSpan.style.opacity = 0
-            activeBackSpan.style.opacity = 0.3
-            passiveTitleSpan.style.opacity = 0
-            backButton.style.opacity = 0
+
+            if (handleNavBar) {
+                activeTitleSpan.style.opacity = 0
+                activeBackSpan.style.opacity = 0.3
+                passiveTitleSpan.style.opacity = 0
+                backButton.style.opacity = 0
+            }
         }
 
         setTimeout( _ => {
             activeView.style.transition = 'transform 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-            activeBackSpan.style.transition = 'transform 0.2s 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.2s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-            activeTitleSpan.style.transition = 'transform 0.2s  cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.4s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-            backButton.style.transition = 'opacity 0.4s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+            
+            if (handleNavBar) {
+                activeBackSpan.style.transition = 'transform 0.2s 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.2s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+                activeTitleSpan.style.transition = 'transform 0.2s  cubic-bezier(0.25, 0.46, 0.45, 0.94), opacity 0.4s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+                backButton.style.transition = 'opacity 0.4s 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+                backButton.style.opacity = 1
+            }
 
             const $shadowPane = passiveView.shadowRoot.querySelector('.shadow-pane')
             $shadowPane.style.display = ''
 
-            backButton.style.opacity = 1
-            
             setTimeout ( _ => {
                 if (this.transition == 'cover-vertical') {
                     activeView.style.transform = 'translateY(0)'
@@ -158,9 +188,13 @@ export class StackNavigator {
                     passiveView.style.transform = 'translateX(-30%)'
                     $shadowPane.style.opacity = 0.4
                     activeView.style.transform = 'translateX(0)'
-                    activeTitleSpan.style.opacity = 1
-                    activeBackSpan.style.transform = 'translateX(0%)'
-                    activeBackSpan.style.opacity = 1
+                    
+                    if (handleNavBar) {
+                        activeTitleSpan.style.opacity = 1
+                        activeBackSpan.style.transform = 'translateX(0%)'
+                        activeBackSpan.style.opacity = 1
+                    }
+                    
                     setTimeout( _ => {
                         passiveView.style.display = 'none'
                     },500)
@@ -177,19 +211,25 @@ export class StackNavigator {
         const $shadowPane = passiveView.shadowRoot.querySelector('.shadow-pane')
         passiveView.style.display = ''
 
-        const navBarPassiveView = passiveView.querySelector('scell-navigation-bar')
-        const navBarActiveView = activeView.querySelector('scell-navigation-bar')
-        const activeTitleSpan = navBarActiveView.shadowRoot.querySelector('.center-zone > span')
-        const activeBackSpan = navBarActiveView.shadowRoot.querySelector('.back-zone > span')
-        const passiveTitleSpan = navBarPassiveView.shadowRoot.querySelector('.center-zone > span')
-        const passiveBackSpan = navBarPassiveView.shadowRoot.querySelector('.back-zone > span')
-        const backButton = navBarPassiveView.leftSlotAssignedNode
+        let backButton,passiveBackSpan,passiveTitleSpan,activeBackSpan,activeTitleSpan,navBarPassiveView,navBarActiveView;
 
-        if (!this.transition == 'cover-vertical') {
-            passiveTitleSpan.style.opacity = 0
-            passiveBackSpan.style.transform = 'translateX(-50%)'
-            activeBackSpan.style.opacity = 0.3
-            backButton.style.opacity = 0
+        const handleNavBar = previousState && handleNavigationBar(previousState)
+        
+        if (handleNavBar) {
+            navBarPassiveView = passiveView.querySelector('scell-navigation-bar')
+            navBarActiveView = activeView.querySelector('scell-navigation-bar')
+            activeTitleSpan = navBarActiveView.shadowRoot.querySelector('.center-zone > span')
+            activeBackSpan = navBarActiveView.shadowRoot.querySelector('.back-zone > span')
+            passiveTitleSpan = navBarPassiveView.shadowRoot.querySelector('.center-zone > span')
+            passiveBackSpan = navBarPassiveView.shadowRoot.querySelector('.back-zone > span')
+            backButton = navBarPassiveView.leftSlotAssignedNode
+
+            if (!this.transition == 'cover-vertical') {
+                passiveTitleSpan.style.opacity = 0
+                passiveBackSpan.style.transform = 'translateX(-50%)'
+                activeBackSpan.style.opacity = 0.3
+                backButton.style.opacity = 0
+            }
         }
 
         setTimeout( _ => {
@@ -199,19 +239,23 @@ export class StackNavigator {
                 activeView.style.opacity = 0
             
             } else {
-                activeBackSpan.style.opacity = 0
-                passiveTitleSpan.style.opacity = 1
                 activeView.style.transform = 'translateX(100%)'
                 passiveView.style.transform = 'translateX(0%)'
-                passiveBackSpan.style.transform = 'translateX(0%)'
                 $shadowPane.style.opacity = 0
-                backButton.style.opacity = 1
+
+                if (handleNavBar) {
+                    activeBackSpan.style.opacity = 0
+                    passiveTitleSpan.style.opacity = 1
+                    passiveBackSpan.style.transform = 'translateX(0%)'
+                    backButton.style.opacity = 1
+                }
+                
                 setTimeout( _ =>  {
                     $shadowPane.style.display = 'none'
                 },400)
             }
 
-            if (!previousControllerClass) {
+            if (handleNavBar) {
                 const navbar = this.passiveController.render().querySelector('scell-navigation-bar')
                 navbar.leftSlotAssignedNode.hide()
             }
@@ -222,8 +266,12 @@ export class StackNavigator {
                 if (previousControllerClass) {
                     this.passiveController = new previousControllerClass(previousState.props)
                     this.passiveController.router = this
-                    this.insertNavigationBarForPop()
-                    this.passiveController.render().style.paddingTop = getComputedStyle(navBarPassiveView).height
+
+                    if (handleNavBar) {
+                        this.insertNavigationBarForPop()
+                        this.passiveController.render().style.paddingTop = getComputedStyle(navBarPassiveView).height
+                    }
+                    
                     document.body.insertBefore(this.passiveController.render(),this.activeController.render())
                     
                     if (this.transition == 'cover-vertical') {
