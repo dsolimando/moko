03497469b247bcdb180f668ad830bd791e623abb
Solimando Damien <dsolimando@gmail.com>
2018-04-10 13:10:46 +0200

    Update demos

--- demo/app/simpleViewController.js
+++ demo/app/simpleViewController.js
@@ -1,23 +1,13 @@
-const $template  = document.querySelector('#view1')
-const $template4  = document.querySelector('#view4')
-const $template3 = document.querySelector('#view3')
+const $template  = document.querySelector('template')
 
-export class BlackSimpleViewController  {
+export class SimpleViewController  {
 
-    constructor() {
+    constructor(props) {
         this.$el = $template.content.cloneNode(true).firstElementChild
-        this.$el.style.background = 'yellow'
+        this.$el.style.background = props.color
 
         this.$el.querySelector('button').onclick = event => {
-            this.$el.hide()
-            setTimeout (_ => {this.$el.remove()},500)
-        }
-
-        this.$el.querySelector('#b2').onclick = event => {
-            navigator.push({
-                viewController:RedSimpleViewController,
-                title:"Red"
-            })
+            this.navigator.pop()
         }
     }
 
@@ -26,91 +16,12 @@ export class BlackSimpleViewController  {
     }
 }
 
-export class RedSimpleViewController  {
+export class EmptyViewController {
 
-    constructor() {
-        this.$el = $template.content.cloneNode(true).firstElementChild
-        this.$el.style.background = 'red'
-
-        this.$el.querySelector('button').onclick = event => {
-            this.$el.hide()
-            setTimeout (_ => {this.$el.remove()},500)
-        }
-    }
-
-    render () {
-        return this.$el
-    }
-}
-
-export class GreenSimpleViewController  {
-
-    constructor() {
-        this.$el = $template.content.cloneNode(true).firstElementChild
-        this.$el.style.background = 'green'
-
-        this.$el.querySelector('button').onclick = event => {
-            this.$el.hide()
-            setTimeout (_ => {this.$el.remove()},500)
-        }
-    }
-
-    render () {
-        return this.$el
-    }
-}
-
-export class View4Controller {
-
-    constructor() {
-        this.$el = $template4.content.cloneNode(true)
-        this.$el.querySelector('scell-view').style.background = '#E2CFEA'
-        this.navigationBar = this.$el.querySelector('scell-navigation-bar')
-        this.ham = this.navigationBar.querySelector('scell-hamburger-button')
-        
-        this.drawer = this.$el.querySelector('scell-drawer')
-        this.ham.addEventListener('click', event => {
-            this.drawer.open()
-                })
-        
-        this.drawer.onclick = event => {
-             this.drawer.close()
-        }
-    }
-
-    render () {
-        return this.$el
-    }
-}
-
-export class View3ControllerBlack {
+    constructor(props) {
+        this.$el = $template.content.cloneNode(true)
+        this.$el.querySelector('scell-view').style.background = props.color || '#E2CFEA'
     
-    constructor() {
-        this.$el = $template3.content.cloneNode(true).firstElementChild
-        this.$el.style.background = '#000'
-        this.$el.querySelector('#b2').onclick = event => {
-            this.navigator.push({
-                viewController:View3ControllerRed
-            })
-        }
-        this.$el.querySelector('button').style.display = 'none'
-    }
-
-    render () {
-        return this.$el
-    }
-} 
-
-export class View3ControllerRed {
-    constructor() {
-        this.$el = $template3.content.cloneNode(true).firstElementChild
-        this.$el.style.background = 'red'
-        this.$el.querySelector('button').onclick = event => {
-            this.navigator.push({
-                viewController:View3ControllerBlack
-            })
-        }
-        this.$el.querySelector('#b2').style.display = 'none'
     }
 
     render () {
