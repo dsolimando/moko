465dbde47cec20dcad0e81e549f94ceb68745ab0
Solimando Damien <dsolimando@gmail.com>
2019-10-26 22:37:28 +0200

    Bump up version to 2.0.0

--- components/text-input/input.js
+++ components/text-input/input.js
@@ -4,30 +4,34 @@ import searchIconSvg from './search-icon.svg'
 const $template = document.createElement('template')
 $template.innerHTML = template
 
-
 class TextInput extends HTMLElement {
+  constructor() {
+    super()
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+    shadowRoot.appendChild($template.content.cloneNode(true))
+  }
 
-    constructor() {
-        super()
-        const shadowRoot = this.attachShadow({mode:'open'})
-        shadowRoot.appendChild($template.content.cloneNode(true))
+  connectedCallback() {
+    const icon = this.getAttribute('icon') || ''
+
+    if (icon && icon == 'search') {
+      const color = getComputedStyle(this).color
+      this.shadowRoot
+        .querySelector('label')
+        .style.setProperty(
+          '--icon-svg',
+          `url("data:image/svg+xml,${encodeURI(
+            searchIconSvg.replace('currentColor', color)
+          )}`
+        )
     }
+  }
 
-    connectedCallback() {
-
-        const icon = this.getAttribute('icon') || ''
-
-        if (icon && icon == 'search' ) {
-            const color = getComputedStyle(this).color
-            this.shadowRoot.querySelector('label').style.setProperty('--icon-svg',`url("data:image/svg+xml,${encodeURI(searchIconSvg.replace('currentColor',color))}`)
-        }
-    }
-
-    get value () {
-        return this.shadowRoot.querySelector('input').value
-    }
+  get value() {
+    return this.shadowRoot.querySelector('input').value
+  }
 }
 
-customElements.define('moko-text-input',TextInput)
+customElements.define('moko-text-input', TextInput)
 
 export default TextInput
